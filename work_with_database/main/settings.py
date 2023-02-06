d1fd072728993070fd2be506a8adbadf42cabad6
Remezov Dmitriy <darknessdizi@gmail.com>
2023-02-06 17:18:50 +0700

    Add class Phone to models.py

--- work_with_database/main/settings.py
+++ work_with_database/main/settings.py
@@ -55,7 +55,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'ваш пароль',
+        'PASSWORD': '1qaz2wsx',
     }
 }
 
@@ -82,8 +82,7 @@ ROOT_URLCONF = 'main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
