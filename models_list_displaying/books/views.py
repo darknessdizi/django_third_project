d6c47c2388a1a95ca10de3c2adf9dc706026b7ef
Remezov Dmitriy <darknessdizi@gmail.com>
2023-02-09 02:26:34 +0700

    Решена вторая задача

--- models_list_displaying/books/views.py
+++ models_list_displaying/books/views.py
@@ -10,7 +10,7 @@ def books_view(request):
 
 
 def books_catalog(request):
-    template = 'books/books_list_new.html'
+    template = 'books/books_catalog.html'
     list_books = []
     for object in Book.objects.all():
         object.pub_date = object.pub_date.strftime('%Y-%m-%d')
@@ -20,7 +20,30 @@ def books_catalog(request):
 
 
 def book_instance(request, data):
-    # page_number = int(request.GET.get("page", 1))
-    # paginator = Paginator(object_list, 10)
-    # page = paginator.get_page(page_number)
-    pass
+    list_books = []
+    list_data = []
+    next_date = None
+    previous_data = None
+
+    for object in Book.objects.all():
+        object.pub_date = object.pub_date.strftime('%Y-%m-%d')
+        list_books.append(object)
+        list_data.append(object.pub_date)
+
+    index = list_data.index(data)
+
+    paginator = Paginator(list_books, 1)
+    page = paginator.get_page(index + 1)
+
+    if index != len(list_data) - 1:
+        next_date = list_data[index + 1]
+    if index != 0:
+        previous_data = list_data[index - 1]
+
+    context = {
+                'books': page,
+                'page': page,
+                'next_date': next_date,
+                'previous_data': previous_data,
+            }  
+    return render(request, 'books/books_catalog.html', context)
